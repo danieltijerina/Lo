7db888eda969e7bc51c32027dd600a4d8aacd922
danieltijerina <dtg.danieltijerina@gmail.com>
2020-10-24 13:00:23 -0500

    Added some add/find logic for symbols table.

--- Semantica/VarTabl.ml
+++ Semantica/VarTabl.ml
@@ -17,6 +17,8 @@ type var_y_func =
 type clase = {
   name : string;
   tipo : string;
+  funcs : (string, funcion) Hashtbl.t;
+  vars : (string, variable) Hashtbl.t;
   dep : (string, var_y_func) Hashtbl.t;
 }
 
@@ -35,9 +37,36 @@ let get_element x =
   | Clase clase -> clase.name
   | Func funcion -> funcion.name;;
 
-add_element has_tab "main" (Clase {name="nombre"; tipo="int"; dep=Hashtbl.create 0;});;
-let x = Hashtbl.find has_tab "main" in 
-  print_string (get_element x);;
+let add_var parent key value = 
+  match parent with 
+  | Func funcion -> Hashtbl.add funcion.variables key value
+  | Clase clase -> Hashtbl.add clase.vars key value;;
+
+let find_var parent key =
+  match parent with
+  | Func funcion -> Hashtbl.find funcion.variables key
+  | Clase clase -> Hashtbl.find clase.vars key;;
+
+let add_func parent key value =
+  match parent with
+  | Clase clase -> Hashtbl.add clase.funcs key value
+  | _ -> assert false;;
+
+let find_func parent key =
+  match parent with
+  | Clase clase -> Hashtbl.find clase.funcs key
+  | _ -> assert false;;
+
+add_element has_tab "figura" (Clase {name="figura"; tipo="int"; funcs=Hashtbl.create 123; vars=Hashtbl.create 123; dep=Hashtbl.create 123;});;
+(*let x = Hashtbl.find has_tab "main" in 
+  print_endline (get_element x);;*)
+
+add_var (Hashtbl.find has_tab "figura") "i" {name="i"; tipo="float";};;
+let var_name = find_var (Hashtbl.find has_tab "figura") "i";;
+print_string var_name.name; print_string " "; print_endline var_name.tipo;;
+
+add_func (Hashtbl.find has_tab "figura") "getColor" {name="getColor"; tipo="string"; variables=Hashtbl.create 123;};;
+print_endline (find_func (Hashtbl.find has_tab "figura") "getColor").tipo;;
 
 (* 
 type var_tbl = {
