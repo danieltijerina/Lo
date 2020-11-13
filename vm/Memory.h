730e5abb5a28ee938090f80aadce16c7d33e9904
danieltijerina <dtg.danieltijerina@gmail.com>
2020-11-13 11:27:30 -0600

    Created vm (not working yet).

---
+++ vm/Memory.h
@@ -0,0 +1,23 @@
+// Memory.h
+#ifndef MEMORY_H // include guard
+#define MEMORY_H
+#include <string>
+
+class Memory {
+public:
+    int* integers;
+    float* floats;
+    std::string* strings;
+    char* chars;
+    bool* booleans;
+
+    Memory(int i, int f, int s, int c, int b) {
+        integers = new int[i];
+        floats = new float[f];
+        strings = new std::string[s];
+        chars = new char[c];
+        booleans = new bool[b];
+    }
+};
+
+#endif /* MEMORY_H */
